35c1c3cde2a8dd974bb9d16b7d3dee18a59e5b4b
Sergey Tesalovskiy <tesser.99.2012@gmail.com>
2020-02-08 20:45:49 +0300

    Added Toolbar, navigation: Back button and ViewPager. Also some icons

--- app/src/main/java/com/tsa/EventMe/MainActivity.java
+++ app/src/main/java/com/tsa/EventMe/MainActivity.java
@@ -1,18 +1,23 @@
 package com.tsa.EventMe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
@@ -25,9 +30,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         auth = FirebaseAuth.getInstance();
         Toast.makeText(this, auth.getUid(), Toast.LENGTH_LONG).show();
-        if(getIntent().getExtras() != null) {
+        if (getIntent().getExtras() != null) {
             String possibleEmail = getIntent().getExtras().getString("email");
-            if( possibleEmail!= null) {
+            if (possibleEmail != null) {
                 //Create new Person
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference ref = database.getReference();
@@ -39,13 +44,11 @@ public class MainActivity extends AppCompatActivity {
                 generateData();
             }
         }
-
-
-
-
-
-
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
 
 
     }
@@ -56,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
         String image1 = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.forbes.com%2Fsites%2Fmarkgreene%2F2019%2F10%2F11%2Fhow-to-buy-a-house-with-10000%2F&psig=AOvVaw21g7PCnoitEJ4uGWGesgSt&ust=1581243678106000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCNmfDdwecCFQAAAAAdAAAAABAD";
         String image2 = "https://www.google.com/url?sa=i&url=http%3A%2F%2Fmediabitch.ru%2Fevent-details%2F&psig=AOvVaw1HpesCLwac2NcLOuTAL8FA&ust=1581246229426000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJDEjLHnwecCFQAAAAAdAAAAABAJ";
 
-        Event event1 = new Event(auth.getCurrentUser().getUid(), auth.getCurrentUser().getEmail(),"Party in John's house", "Really big night party at John", image1, "Nijniy Novgorod", new GregorianCalendar(2020,2, 7));
-        Event event2 = new Event(auth.getCurrentUser().getUid(), auth.getCurrentUser().getEmail(),"Music concert", "All favourites here! Lets go!", image2, "Nijniy Novgorod", new GregorianCalendar(2020,2, 17));
+        Event event1 = new Event(auth.getCurrentUser().getUid(), auth.getCurrentUser().getEmail(), "Party in John's house", "Really big night party at John", image1, "Nijniy Novgorod", new GregorianCalendar(2020, 2, 7));
+        Event event2 = new Event(auth.getCurrentUser().getUid(), auth.getCurrentUser().getEmail(), "Music concert", "All favourites here! Lets go!", image2, "Nijniy Novgorod", new GregorianCalendar(2020, 2, 17));
 
         database = FirebaseDatabase.getInstance();
 
@@ -69,4 +72,45 @@ public class MainActivity extends AppCompatActivity {
         allEvents.push().setValue(event2);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_account:
+                startActivity(new Intent(this, ProfileActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public int getCount() {
+            return 2;
+        }
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new AllEventsFragment();
+                case 1:
+                    return new MyEventsFragment();
+            }
+            return null;
+        }
+    }
+
 }
